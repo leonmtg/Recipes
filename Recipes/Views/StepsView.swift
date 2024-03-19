21ac686053133b254482d114b601a3e37cffc3df
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2024-03-19 15:02:06 +0100

    added editMode to StepsView

--- Recipes/Views/StepsView.swift
+++ Recipes/Views/StepsView.swift
@@ -18,8 +18,22 @@ struct StepsView: View {
         List {
             ForEach(recipe.viewSortedSteps) { step in
                 @Bindable var step = step
-                Label(step.instruction, systemImage: "\(step.stepNumber).square")
+                if editMode {
+                    VStack {
+                        Stepper(value: $step.stepNumber) {
+                            Image(systemName: "\(step.stepNumber).square")
+                                .font(.title)
+                        }
+                        
+                        TextField("instruction", text: $step.instruction, axis: .vertical)
+                            .lineLimit(10)
+                    }
+                    .id(step.stepNumber)
+                } else {
+                    Label(step.instruction, systemImage: "\(step.stepNumber).square")
+                }
             }
+            .onDelete(perform: editMode ? delete : nil)
             .listRowBackground(Color.primary.opacity(0.05))
         }
         .scrollContentBackground(.hidden)
@@ -36,10 +50,29 @@ struct StepsView: View {
             }
         }
         .navigationTitle("Steps")
+        .toolbar {
+            if editMode {
+                Button("", systemImage: "plus") {
+                    let stepNumber = recipe.steps.count+1
+                    recipe.steps.append(
+                        Step(
+                            stepNumber: stepNumber,
+                            instruction: ""
+                        )
+                    )
+                }
+            }
+        }
+    }
+    
+    func delete(indexSet: IndexSet) {
+        for index in indexSet {
+            modelContext.delete(recipe.viewSortedSteps[index])
+        }
     }
 }
 
-#Preview {
+#Preview("Normal Mode") {
     @Dependency(\.database) var database
     let container = database.modelContainer()
     
@@ -50,5 +83,19 @@ struct StepsView: View {
     
     return NavigationStack {
         StepsView(recipe: recipes[0])
+    }
+}
+
+#Preview("Edit Mode") {
+    @Dependency(\.database) var database
+    let container = database.modelContainer()
+    
+    let recipes = try! container.mainContext.fetch(
+        FetchDescriptor<Recipe>(predicate: #Predicate { recipe in
+            recipe.name == "Lobster Bisque"
+        }))
+    
+    return NavigationStack {
+        StepsView(recipe: recipes[0], editMode: true)
     }
 }
